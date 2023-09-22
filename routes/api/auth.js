df010b6337c0e1efd96f297b4253447e271315b8
endorf8n <volodymyr.konopka@gmail.com>
2023-09-22 13:54:56 +0300

    user controller, model, route, schema

---
+++ routes/api/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth");
+
+const { validateBody } = require("../../decorators");
+
+const schemas = require("../../schemas/users");
+
+const router = express.Router();
+
+const registerValidateMiddleware = validateBody(schemas.userRegisterSchema);
+const loginValidateMiddleware = validateBody(schemas.userLoginSchema);
+
+router.post("/register", registerValidateMiddleware, ctrl.register);
+
+module.exports = router;
