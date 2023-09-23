6f8a189dbcae530d294c342e5d55fe5ad3986c30
Dima <vidistudioproduction@gmail.com>
2023-09-23 15:26:35 +0300

    create controllers and routes next

--- controllers/notices.js
+++ controllers/notices.js
@@ -3,7 +3,6 @@ const { ctrlWrapper } = require("../decorators");
 const { noticeAddSchema, noticeAddSellSchema } = require("../schemas/notices");
 const { HttpError, cloudinaryUploader } = require("../helpers");
 const fs = require("fs/promises");
-const { log } = require("console");
 
 const getAllNotices = async (req, res, next) => {
   const notices = await Notice.find();
@@ -19,6 +18,21 @@ const getNoticeById = async (req, res) => {
   res.status(200).json({
     notice,
   });
+};
+
+const getUserNotices = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const notices = await Notice.find({ owner }, "", { skip, limit }).populate(
+    "owner",
+    "email"
+  );
+  if (!notices) {
+    throw HttpError(404);
+  }
+  res.status(200).json({page, limit, total: notices.length, notices});
 };
 
 const addNotice = async (req, res) => {
@@ -81,10 +95,47 @@ const getNoticesInFavorites = async (req, res) => {
   res.status(200).json({page, limit, total: notices.length, notices});
 };
 
+const removeNoticeFavorites = async (req, res) => {
+  const { noticeId } = req.params;
+  const { _id } = req.user;
+  const notice = await Notice.findByIdAndUpdate(
+    noticeId,
+    { $pull: { favorites: _id } },
+    {
+      new: true,
+    }
+  );
+  if (!notice) {
+    throw HttpError(404);
+  }
+  res.status(200).json(notice);
+}
+
+const removeNotice = async (req, res) => {
+  const { noticeId } = req.params;
+  const { _id: owner } = req.user;
+  const notice = await Notice.findById(noticeId);
+  if (!notice) {
+    throw HttpError(404);
+  }
+
+  if (notice.owner.toString() !== owner.toString()) {
+    throw HttpError(403, 'You are not allowed to delete this notice')
+  }
+  await Notice.findByIdAndDelete(noticeId);
+
+  res.status(200).json({
+    message: 'Success your notice deleted'
+  })
+}
+
 module.exports = {
   getAllNotices: ctrlWrapper(getAllNotices),
   addNotice: ctrlWrapper(addNotice),
   getNoticeById: ctrlWrapper(getNoticeById),
   updateNoticeFavorites: ctrlWrapper(updateNoticeFavorites),
   getNoticesInFavorites: ctrlWrapper(getNoticesInFavorites),
+  removeNoticeFavorites: ctrlWrapper(removeNoticeFavorites),
+  getUserNotices: ctrlWrapper(getUserNotices),
+  removeNotice: ctrlWrapper(removeNotice),
 };
--- routes/api/notices.js
+++ routes/api/notices.js
@@ -6,7 +6,11 @@ const { authenticate, upload, isValidId } = require('../../middlewares');
 router.get('/', ctrl.getAllNotices);
 router.get('/:noticeId', isValidId, ctrl.getNoticeById);
 router.get("/favorites/v2", authenticate, ctrl.getNoticesInFavorites);
+router.get('/user-notices/v2', authenticate, ctrl.getUserNotices);
 router.post('/', authenticate, upload.single('file'), ctrl.addNotice);
-router.patch('/:noticeId/favorites', authenticate, isValidId, ctrl.updateNoticeFavorites);
+router.patch('/:noticeId/favorites-add', authenticate, isValidId, ctrl.updateNoticeFavorites);
+router.patch('/:noticeId/favorites-del', authenticate, isValidId, ctrl.removeNoticeFavorites);
+router.delete('/:noticeId', authenticate, isValidId, ctrl.removeNotice);
+
 
 module.exports = router;
