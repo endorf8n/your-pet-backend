a896cfcaaafc1c2caa37c31403961706cb2f33ca
Dima <vidistudioproduction@gmail.com>
2023-10-02 00:30:28 +0300

    Update buildPaginationOptions.js

--- helpers/buildPaginationOptions.js
+++ helpers/buildPaginationOptions.js
@@ -1,6 +1,11 @@
+const HttpError = require('./HttpError');
+
 const buildPaginationOptions = (query) => {
   const { page = 1, limit = 12 } = query
   const skip = (page - 1) * limit;
+  if (skip < 0) {
+    throw HttpError(400, "Page cannot be a negative number")
+  }
   return { skip, page, limit };
 }
 
